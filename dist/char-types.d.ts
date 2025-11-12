703a69e1f56101274a03e9daba815a78de8f760b
Bart Veneman <bartveneman@gmail.com>
2025-11-12 20:17:48 +0100

    fix pkg name+version and rm dist

--- dist/char-types.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function isDigit(ch: number): boolean;
-export declare function isHexDigit(ch: number): boolean;
-export declare function isAlpha(ch: number): boolean;
-export declare function isWhitespace(ch: number): boolean;
-export declare function isNewline(ch: number): boolean;
-export declare function isIdentStart(ch: number): boolean;
-export declare function isIdentChar(ch: number): boolean;
